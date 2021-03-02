459ea8ec5d0345fa8ae9c082fb6a7be8e1456015
1712593975@qq.com <131415217.Jxy>
2021-03-02 19:47:24 +0800

    密碼修改 改走加密通道 	 凍結后該用戶不能登錄 -判斷未寫（走Redis登錄判斷） 	 图片上传与普通属性上传分开写（接口分开） 	 队员列表编辑窗口赋值 	 队员列表分页 	 队员列表模糊查询(加入一个线程玩玩) 	 将角色信息存与权限信息存入redis(角色名字+权限信息) 	 每小时更新一次redis中的角色与权限信息 	 登录角色只能查看自己拥有的权限(菜单->没有写) 	 发起访问接口时 该角色的权限存入redis 	 优化数据库的权限value列      403前端(弹出无权限窗口)      	ü 用户注册 (图片未写)
         	2021年3月1日:
         日报模块

--- static/layui/js/team-list.js
+++ static/layui/js/team-list.js
@@ -1,46 +1,17 @@
 $(function () {
     findAll();
 })
-let NowUserId = 0;//初始化一个点击后当前用户id
-let status = 0;//初始化冻结状态id
 function findAll() {
-    let data = {
-        username: $("#username").val(),
-        role: $("#role").val(),
-        sex: $("#sex").val(),
-        phone: $("#phone").val(),
-        address: $("#address").val(),
-        createTime: $("#createTime").val(),
-        curPage: $("#curPage").val(),
-        pageSize: $("#pageSize").val(),
-        startDate:$("#start").val(),
-        endDate:$("#end").val()
-    };
-    // console.log(data.startDate,'startDate')
-    let result = myAjax('/user/findPlayerAll', data, 'get');
-    $("#totalPage").html(result.totalCount);
+
+    let result = myAjax('/team/findTeamAll', data, 'get');
     setData(result.result);// 赋值用
 };
-
 // 循环赋值开始
 function setData(data) {
     // console.log(data,'data')
     let html = '';
-    for (let i = 0; i < data.length; i++) {
-        if (data[i].status === 1) {
-            data[i].status = '' + '已启用'     
-        } else {
-            data[i].status = '' + '已冻结'
-        }
-        ;
-        // 队员头像上传地址
-        let imgPath = "static/layui/images/"
         html += '<tr>\n' +
-            // '     <td >\n' +
-            // '       <input type="checkbox" name="id" value="1"   lay-skin="primary"> \n' +// ID多选
-            // '     </td>\n' +
             '     <td >' + data[i].id + '</td>\n' +
-            '     <td> <img src="'+imgPath + data[i].icon + '" width="66" height="66"></td>\n' +
             '     <td>' + data[i].username + '</td>\n' +
             '     <td>' + data[i].role + '</td>\n' +
             '     <td>' + data[i].sex + '</td>\n' +
@@ -55,9 +26,6 @@ function setData(data) {
             '     </td>\n' +
             '   </tr>'
         // ; console.log(data[i].id+'::data')
-    }
-
-
     $("#manageLoopList").html(html);
 
 }
